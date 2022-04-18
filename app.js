53c51e8193cb18118d59dada6376ac1dcf6088e3
Guilherme Lima <guilherme.lima@alura.com>
2022-04-18 14:46:55 -0300

    voltando para o estado do paulo

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectado ao banco de dados")
 // mais um console log de teste:
 console.log("O paulo fez esse commit sem voce ver na aula!")
-console.log("Enviando para o GitHub")
 
 
