a0c893b59a823279cec4200d35c23121349239cd
Paulo Silveira <paulo.silveira@caelum.com.br>
2022-04-18 14:33:38 -0300

    console log indicando modificacoes do paulo

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectado ao banco de dados")
+// mais um console log de teste:
+console.log("O paulo fez esse commit sem voce ver na aula!")
+
 
